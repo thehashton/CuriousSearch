40ee30dc93276d563b76900a0d3ac217396e6f5c
Harry Ashton <hghazni@gmail.com>
2023-06-27 00:50:36 +0100

    Adds search results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,8 @@ export default function Home() {
       <div className="z-10 w-full max-w-md items-center justify-between font-mono text-sm lg:flex-inline">
         <h1 className={'text-5xl my-10'}>Curious Search</h1>
         <Search onSearch={handleSearch} />
+          <h2 className={'text-2xl mt-20 mx-2 underline'}>Searched for:</h2>
+          <p className={'text-2xl m-2'}> {searchValue}</p>
       </div>
     </main>
   )
